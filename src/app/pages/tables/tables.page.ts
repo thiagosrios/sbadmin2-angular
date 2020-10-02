7d11b453ff10fcb5a67178198d0c4874327681b5
Thiago de Souza Rios <thiagosrios@gmail.com>
2020-10-01 21:39:48 -0300

    Correção no uso do plugin DataTables

--- src/app/pages/tables/tables.page.ts
+++ src/app/pages/tables/tables.page.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+declare var $: (arg0: any) => any; // jQuery
 
 @Component({
   selector: 'app-tables',
   templateUrl: './tables.page.html',
   styleUrls: ['./tables.page.scss']
 })
-export class TablesPage implements OnInit {
+export class TablesPage {
+
+  @ViewChild('dataTable') table: any;
+  public dataTable: any;
+  public $: any; 
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    console.log(this.table);
+    this.dataTable = $(this.table.nativeElement);
+    this.dataTable.DataTable();
   }
 
 }
